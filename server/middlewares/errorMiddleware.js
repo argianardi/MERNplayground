14364dc72d7caf8f4f9ba451d3901938579c2660
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-21 21:36:25 +0700

    Feat: Add error handling middleware

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -5,11 +5,30 @@ export const notFound = (req, res, next) => {
 };
 
 export const errorHandler = (err, req, res, next) => {
+  // Jika status kode respon saat ini adalah 200 (servernya erorr tapi statusnya 200), kembalikan status kode respon 500
   const resStatusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
+  // Cek apakah error adalah validation error dari mongoose
+  if (err.name === 'ValidationError') {
+    const formattedErrors = Object.keys(err.errors).reduce((acc, key) => {
+      acc[key] = [err.errors[key].message];
+      return acc;
+    }, {});
+
+    return res.status(400).json({
+      code: '400',
+      status: 'error',
+      message: 'Validation error',
+      errors: formattedErrors,
+    });
+  }
+
+  // default error response
   res.status(resStatusCode).json({
-    message,
+    code: resStatusCode.toString(),
+    status: 'error',
+    message: message,
     stack: err.stack,
   });
 };
