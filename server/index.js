af307d9a6ca9866d542247157702ed2e25b71632
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-21 13:47:01 +0700

    Feat: Add error handling

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+import authRouter from './routes/authRouter.js';
+import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
+// Route
+app.use('/api/v1/auth', authRouter);
+
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandler);
+
+// Connect DB
+connectDB();
+
+// Port
+const PORT = process.env.PORT || 3010;
+
+// Server
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
