0e249fbfbcc6d1d578ba11c25b3102367e543723
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-21 21:36:52 +0700

    Feat: Add async handler

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import User from '../models/users.js';
+import asyncHandler from '../middlewares/asyncHandler.js';
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-  res.send('register');
-});
+router.post(
+  '/register',
+  asyncHandler(async (req, res) => {
+    await User.create({
+      name: req.body.name,
+    });
+  })
+);
 
 router.post('/login', (req, res) => {
   res.send('Login');
