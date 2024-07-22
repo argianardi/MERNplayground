39dc61d2af6e6a89bcb5833ca6c30c7f3f9b8bc8
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-22 11:33:56 +0700

    Fix: Fix user validation

--- server/models/users.js
+++ server/models/users.js
@@ -1,18 +1,37 @@
 import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new Schema({
   name: {
     type: String,
     required: [true, 'Name is required'],
+    unique: [true, 'Name must be unique'],
   },
   email: {
     type: String,
     required: [true, 'Email is required'],
+    unique: [true, 'Email must be unique'],
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email',
+    },
   },
   password: {
     type: String,
     required: [true, 'Password is required'],
+    minLength: [6, 'Password must be at least 6 characters long'],
   },
+  role: {
+    type: String,
+    enum: ['user', 'owner'],
+    default: 'user',
+  },
+});
+
+userSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model('User', userSchema);
