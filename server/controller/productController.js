95982eeddc9e90ac36cd3248e1d58e8c928180f4
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-23 16:44:44 +0700

    Feat: Add get all products and get product by id

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import asyncHandler from '../middlewares/asyncHandler.js';
 import Product from '../models/products.js';
 
@@ -9,5 +10,37 @@ export const CreateProduct = asyncHandler(async (req, res) => {
     status: 'Scuccess',
     message: 'Product created successfully',
     data: newProduct,
+  });
+});
+
+export const GetAllProduct = asyncHandler(async (req, res) => {
+  const products = await Product.find();
+
+  return res.status(200).json({
+    code: '200',
+    status: 'Success',
+    message: 'Request was successful',
+    data: products,
+  });
+});
+
+export const GetProductById = asyncHandler(async (req, res) => {
+  if (!mongoose.isValidObjectId(req?.params?.id)) {
+    res.status(404);
+    throw new Error('Invalid ID format');
+  }
+
+  const product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    res.status(404);
+    throw new Error('ID not found');
+  }
+
+  return res.status(200).json({
+    code: '200',
+    status: 'Success',
+    message: 'Request was successful',
+    data: product,
   });
 });
