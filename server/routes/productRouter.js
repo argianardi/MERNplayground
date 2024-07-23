969069fffc00a3b46d033201191105b921886c0d
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-23 21:52:31 +0700

    Feat: Add protected middleware

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -6,13 +6,14 @@ import {
   GetProductById,
   UpdateProduct,
 } from '../controller/productController.js';
+import { protectedMiddleware } from '../middlewares/authMidleware.js';
 
 const router = express.Router();
 
 router.post('/', CreateProduct);
-router.get('/', GetAllProduct);
-router.get('/:id', GetProductById);
-router.put('/:id', UpdateProduct);
-router.delete('/:id', DeleteProduct);
+router.get('/', protectedMiddleware, GetAllProduct);
+router.get('/:id', protectedMiddleware, GetProductById);
+router.put('/:id', protectedMiddleware, UpdateProduct);
+router.delete('/:id', protectedMiddleware, DeleteProduct);
 
 export default router;
