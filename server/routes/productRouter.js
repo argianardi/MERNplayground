7dcf6d9e2f0e00a012557e3770a3b6c6bc80fa28
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-23 16:45:03 +0700

    Feat:  Add get all products and get product by id

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { CreateProduct } from '../controller/productController.js';
+import {
+  CreateProduct,
+  GetAllProduct,
+  GetProductById,
+} from '../controller/productController.js';
 
 const router = express.Router();
 
 router.post('/', CreateProduct);
+router.get('/', GetAllProduct);
+router.get('/:id', GetProductById);
 
 export default router;
