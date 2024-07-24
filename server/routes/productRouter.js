7fa131c58938bf96c947b809682b29a65d508ffd
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-24 15:34:56 +0700

    Feat: Add file request

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -5,11 +5,13 @@ import {
   GetAllProduct,
   GetProductById,
   UpdateProduct,
+  UploadImage,
 } from '../controller/productController.js';
 import {
   ownerMiddleware,
   protectedMiddleware,
 } from '../middlewares/authMidleware.js';
+import { upload } from '../utils/uploadFileHandler.js';
 
 const router = express.Router();
 
@@ -18,5 +20,12 @@ router.get('/', protectedMiddleware, GetAllProduct);
 router.get('/:id', protectedMiddleware, GetProductById);
 router.put('/:id', protectedMiddleware, ownerMiddleware, UpdateProduct);
 router.delete('/:id', protectedMiddleware, ownerMiddleware, DeleteProduct);
+router.post(
+  '/file-upload',
+  protectedMiddleware,
+  ownerMiddleware,
+  upload.single('image'),
+  UploadImage
+);
 
 export default router;
