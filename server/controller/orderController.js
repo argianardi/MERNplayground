9c909733252ea0fe6d4533bf595ae96ee173d097
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-25 22:00:49 +0700

    Feat: Add order controller

---
+++ server/controller/orderController.js
@@ -0,0 +1,53 @@
+import asyncHandler from '../middlewares/asyncHandler.js';
+import Order from '../models/orderModel.js';
+import Product from '../models/productModel.js';
+
+export const CreateOrder = asyncHandler(async (req, res) => {
+  const { email, firstName, lastName, phone, cartItems } = req?.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    res.status(400);
+    throw new Error('Cart is empty');
+  }
+
+  let orderItems = [];
+  let totalPrice = 0;
+
+  for (const cart of cartItems) {
+    const productData = await Product.findOne({ _id: cart?.product });
+
+    if (!productData) {
+      res.status(404);
+      throw new Error('Product not found');
+    }
+
+    const { name, price, _id } = productData;
+    const singleProduct = {
+      quantity: cart?.quantity,
+      name,
+      price,
+      product: _id,
+    };
+
+    orderItems = [...orderItems, singleProduct];
+    totalPrice += cart?.quantity * price;
+  }
+
+  const order = await Order.create({
+    itemsDetail: orderItems,
+    totalPrice,
+    firstName,
+    lastName,
+    phone,
+    email,
+    user: req?.user?.id,
+  });
+
+  return res.status(201).json({
+    code: '201',
+    status: 'Success',
+    message: 'Order created successfully',
+    // data: { totalPrice: total, orderItems: orderItems },
+    data: order,
+  });
+});
