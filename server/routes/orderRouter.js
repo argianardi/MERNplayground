fc60671cd69b1e32d7864af382c36451ad49984b
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-26 07:37:50 +0700

    Feat: Add get all order, get order by id and get current user order router

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -1,9 +1,20 @@
 import express from 'express';
-import { protectedMiddleware } from '../middlewares/authMidleware.js';
-import { CreateOrder } from '../controller/orderController.js';
+import {
+  ownerMiddleware,
+  protectedMiddleware,
+} from '../middlewares/authMidleware.js';
+import {
+  CreateOrder,
+  CurrentUserOrder,
+  GetAllOrders,
+  GetOrderById,
+} from '../controller/orderController.js';
 
 const router = express.Router();
 
 router.post('/', protectedMiddleware, CreateOrder);
+router.get('/', protectedMiddleware, ownerMiddleware, GetAllOrders);
+router.get('/:id', protectedMiddleware, ownerMiddleware, GetOrderById);
+router.get('/current/user', protectedMiddleware, CurrentUserOrder);
 
 export default router;
