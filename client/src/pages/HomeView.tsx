6717530df5d56594f0b4978ff26f76d8ced38ee4
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-28 09:32:49 +0700

    Feat: Add api fetch

--- client/src/pages/HomeView.tsx
+++ client/src/pages/HomeView.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import axios from 'axios';
+
+try {
+  const data = await axios.get('/api/v1/products');
+  console.log(data);
+} catch (error) {
+  console.log(error);
+}
 
 const HomeView = () => {
   return <div className="text-red-600 font-bold underline">HomeView</div>;
