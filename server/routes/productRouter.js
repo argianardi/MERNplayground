4c911ed74cbf5872a17cda237e1df7e4cde27a03
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-07-30 12:55:48 +0700

    Feat: Add router to upload file to cloudinary

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,31 +1,41 @@
 import express from 'express';
 import {
+  CloudinaryUploadImage,
   CreateProduct,
   DeleteProduct,
   GetAllProduct,
   GetProductById,
+  LocalUploadImage,
   UpdateProduct,
-  UploadImage,
 } from '../controller/productController.js';
 import {
   ownerMiddleware,
   protectedMiddleware,
 } from '../middlewares/authMidleware.js';
-import { upload } from '../utils/uploadFileHandler.js';
-
+import { upload as uploadCloudinary } from '../utils/cloudinaryUploadFileHandler.js';
+import { upload as uploadLocal } from '../utils/localUploadFileHandler.js';
 const router = express.Router();
 
 router.post('/', ownerMiddleware, CreateProduct);
-router.get('/', protectedMiddleware, GetAllProduct);
+router.get('/', GetAllProduct);
+// router.get('/', protectedMiddleware, GetAllProduct);
 router.get('/:id', protectedMiddleware, GetProductById);
 router.put('/:id', protectedMiddleware, ownerMiddleware, UpdateProduct);
 router.delete('/:id', protectedMiddleware, ownerMiddleware, DeleteProduct);
 router.post(
-  '/file-upload',
+  '/file-upload-local',
   protectedMiddleware,
   ownerMiddleware,
-  upload.single('image'),
-  UploadImage
+  uploadLocal.single('image'),
+  LocalUploadImage
+);
+
+router.post(
+  '/file-upload-cloudinary',
+  protectedMiddleware,
+  ownerMiddleware,
+  uploadCloudinary.single('image'),
+  CloudinaryUploadImage
 );
 
 export default router;
