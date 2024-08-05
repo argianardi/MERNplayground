bd80f1246a35636421e755a77f625ff58c2f39b6
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-05 14:24:31 +0700

    Feat: Add stock and category product

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,8 +1,17 @@
 import { Link } from 'react-router-dom';
 import { ProductType } from '../types/ProductTypes';
 import { formatToRupiah } from '../utils/currencyFormatter';
+import { truncateText } from '../utils/truncateText';
 
-const ProductCard = ({ _id, name, image, price, description }: ProductType) => {
+const ProductCard = ({
+  _id,
+  name,
+  image,
+  price,
+  description,
+  stock,
+  category,
+}: ProductType) => {
   return (
     <Link to={`products/${_id}`} className="shadow-xl card bg-base-300">
       <figure>
@@ -11,7 +20,11 @@ const ProductCard = ({ _id, name, image, price, description }: ProductType) => {
       <div className="card-body">
         <h2 className="card-title text-primary">{name}</h2>
         <p className="font-bold text-accent">{formatToRupiah(price)}</p>
-        <p>{description.substring(0, 5)}...</p>
+        <p>{truncateText(description, 5)}</p>
+        <div className="flex items-center justify-between">
+          <p className="font-bold">Stock: {stock}</p>
+          <p className="font-bold">Category: {category}</p>
+        </div>
         <div className="justify-end card-actions">
           <button className="btn btn-primary">Buy Now</button>
         </div>
