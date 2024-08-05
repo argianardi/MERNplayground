39c51a6d594f8ebf6ba86801bd5d4560aef0c5db
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-05 20:58:24 +0700

    Feat: Add search input default value

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -1,16 +1,28 @@
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useLoaderData } from 'react-router-dom';
 import FormInput from './form/FormInput';
 import FormSelect from './form/FormSelect';
 
 const Filter = () => {
   const categories = ['Sepatu', 'Baju', 'Celana'];
+  const { params } = useLoaderData() as { params: { [key: string]: string } };
+  const { name, category } = params;
   return (
     <Form
       method="get"
       className="grid grid-cols-2 px-8 py-4 rounded-md bg-base-200 gap-x-4 gap-y-3"
     >
-      <FormInput label="Search Product" name="name" type="search" />
-      <FormSelect label="Select category" name="category" list={categories} />
+      <FormInput
+        label="Search Product"
+        name="name"
+        type="search"
+        defaultValue={name}
+      />
+      <FormSelect
+        label="Select category"
+        name="category"
+        list={categories}
+        defultValue={category}
+      />
       <button type="submit" className="btn btn-primary">
         Search
       </button>
