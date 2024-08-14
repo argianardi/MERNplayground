5486b904a8b0e440344c688ee31c103ddc53a159
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-14 20:52:48 +0700

    Feat: Add form input component

--- client/src/components/form/FormInput.tsx
+++ client/src/components/form/FormInput.tsx
@@ -1,23 +1,63 @@
+// Tanpa state management
+// interface FormInputType {
+//   defaultValue?: string;
+//   label: string;
+//   type: string;
+//   name: string;
+// }
+
+// const FormInput = ({ label, name, type, defaultValue }: FormInputType) => {
+//   return (
+//     <label className="form-control">
+//       <label className="label">
+//         <span className="capitalize label-text">{label}</span>
+//       </label>
+//       <input
+//         type={type}
+//         name={name}
+//         defaultValue={defaultValue}
+//         className="input input-bordered"
+//       />
+//     </label>
+//   );
+// };
+
+// export default FormInput;
+
+// Menggunakan state management
+import { ChangeEvent } from 'react';
+
 interface FormInputType {
-  defaultValue?: string;
   label: string;
   type: string;
   name: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
 }
 
-const FormInput = ({ label, name, type, defaultValue }: FormInputType) => {
+const FormInput = ({
+  label,
+  name,
+  type,
+  value,
+  onChange,
+  error,
+}: FormInputType) => {
   return (
-    <label className="form-control">
+    <div className="form-control">
       <label className="label">
         <span className="capitalize label-text">{label}</span>
       </label>
       <input
         type={type}
         name={name}
-        defaultValue={defaultValue}
+        value={value}
+        onChange={onChange}
         className="input input-bordered"
       />
-    </label>
+      {error && <span className="text-sm text-red-500">{error}</span>}
+    </div>
   );
 };
 
