38ba095566d73a86a9df8e033fff9c2dff4b76c2
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-14 20:53:47 +0700

    Feat: Add auth api fetch method

---
+++ client/src/services/authService.ts
@@ -0,0 +1,20 @@
+import customAPI from '.';
+
+const authService = async (url: string, data: Record<string, any>) => {
+  console.log(data);
+
+  try {
+    const response = await customAPI.post(url, data);
+    console.log(response);
+
+    return response.data.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw new Error(error?.response.data.message || 'Something went wrong');
+    } else {
+      throw new Error('Network error');
+    }
+  }
+};
+
+export default authService;
