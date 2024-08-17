6b3a89d9ea4e888eb8a0c9faf5c4550dae959a74
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-17 20:12:09 +0700

    Feat: Add register method without cookie

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -2,18 +2,19 @@ import jwt from 'jsonwebtoken';
 import asyncHandler from '../middlewares/asyncHandler.js';
 import User from '../models/userModel.js';
 
+// To create token
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '6d',
   });
 };
 
+// Create and send token use in cookie
 const createSendResToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const isDev = process.env.NODE_ENV === 'development' ? false : true;
   const cookieOption = {
     expire: new Date(Date.now() + 6 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
     security: isDev,
   };
 
@@ -29,6 +30,25 @@ const createSendResToken = (user, statusCode, res) => {
   });
 };
 
+// Create and send token to json response
+const createSendResTokenJson = (user, statusCode, res) => {
+  const token = signToken(user?._id);
+
+  // Delete password from user before send it to client
+  user.password = undefined;
+
+  res?.status(statusCode).json({
+    code: statusCode.toString(),
+    status: 'Success',
+    message: 'User created successfully',
+    data: {
+      user,
+      token,
+    },
+  });
+};
+
+// Register with cookie
 export const registerUser = asyncHandler(async (req, res) => {
   const isOwner = (await User.countDocuments()) === 0;
   const role = isOwner ? 'owner' : 'user';
@@ -41,6 +61,21 @@ export const registerUser = asyncHandler(async (req, res) => {
   });
 
   createSendResToken(createUser, 201, res);
+});
+
+// Register without cookie
+export const registerUserJson = asyncHandler(async (req, res) => {
+  const isOwner = (await User?.countDocuments()) === 0;
+  const role = isOwner ? 'owner' : 'user';
+
+  const createUser = await User?.create({
+    name: req?.body?.name,
+    email: req?.body?.email,
+    password: req?.body?.password,
+    role: role,
+  });
+
+  createSendResTokenJson(createUser, 201, res);
 });
 
 export const loginUser = asyncHandler(async (req, res) => {
@@ -84,7 +119,6 @@ export const getCurrentUser = asyncHandler(async (req, res) => {
 
 export const logoutUser = async (req, res) => {
   res.cookie('jwt', '', {
-    httpOnly: true,
     expires: new Date(Date.now()),
   });
 
