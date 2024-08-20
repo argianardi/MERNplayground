92660d35d293e8bc5970b9657d06d788345cf65c
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-20 17:03:15 +0700

    Feat: Add register component without cookie

---
+++ client/src/pages/auth/RegisterWithoutCookies.tsx
@@ -0,0 +1,11 @@
+import FormAuthWithoutCookies from '../../components/FormAuthWithoutCookies';
+
+const RegisterWithoutCookies = () => {
+  return (
+    <main>
+      <FormAuthWithoutCookies isRegister={true} />
+    </main>
+  );
+};
+
+export default RegisterWithoutCookies;
