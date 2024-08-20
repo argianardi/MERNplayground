72716f5e64aba0ecfaf749a15b65f2d0cc5f3411
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-21 02:18:32 +0700

    Feat: Add product service without cookie

---
+++ client/src/services/productServiceWithoutCookie.ts
@@ -0,0 +1,34 @@
+import { NavigateFunction } from 'react-router-dom';
+import customAPI from '.';
+
+// Get token from local storage
+const token = localStorage.getItem('jwt');
+
+export const getAllProductsWithoutCookie = async (
+  navigate: NavigateFunction
+) => {
+  try {
+    const response = await customAPI.get('/products/without-cookie', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response?.data?.data;
+  } catch (error: any) {
+    if (error?.response) {
+      if (error?.response?.status === 401) {
+        localStorage.removeItem('jwt');
+        navigate('/login-without-cookie');
+
+        throw new Error('Session expired, please log in again');
+      } else {
+        throw new Error(
+          error?.response?.data?.message || 'Something went wrong'
+        );
+      }
+    } else {
+      throw new Error('Network error');
+    }
+  }
+};
