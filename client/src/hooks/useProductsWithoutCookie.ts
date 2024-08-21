4a4e2717fb7f78da05e24fc41b29a3b2a46ee892
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-21 15:12:46 +0700

    Feat: Add page parameter to support pagination

--- client/src/hooks/useProductsWithoutCookie.ts
+++ client/src/hooks/useProductsWithoutCookie.ts
@@ -1,26 +1,41 @@
+// src/hooks/useProductsWithoutCookie.ts
+
 import { useEffect, useState } from 'react';
 import { ProductType } from '../types/ProductTypes';
 import { getAllProductsWithoutCookie } from '../services/productServiceWithoutCookie';
 import { useNavigate } from 'react-router-dom';
 
+interface PaginationType {
+  totalItems: number;
+  pageSize: number;
+  totalPages: number;
+  currentPage: number;
+}
+
 export const useProductsWithoutCookie = (
   name: string | null,
-  category: string | null
+  category: string | null,
+  page: number
 ) => {
   const navigate = useNavigate();
   const [products, setProducts] = useState<ProductType[]>([]);
+  const [pagination, setPagination] = useState<PaginationType | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getAllProducts = async () => {
       try {
-        const data = await getAllProductsWithoutCookie(
+        const data = await getAllProductsWithoutCookie({
           navigate,
           name,
-          category
-        );
-        setProducts(data);
+          category,
+          page,
+          limit: 10,
+        });
+
+        setProducts(data.data);
+        setPagination(data.pagination);
       } catch (error: any) {
         setError(error.message);
       } finally {
@@ -29,7 +44,7 @@ export const useProductsWithoutCookie = (
     };
 
     getAllProducts();
-  }, [name, category, navigate]);
+  }, [name, category, page, navigate]);
 
-  return { products, error, isLoading };
+  return { products, pagination, error, isLoading };
 };
