55edc2388c746618101228cdffba33560c217af9
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-21 15:13:06 +0700

    Feat: Add pagination

--- client/src/pages/pages_without_cookie/ProductViewWithoutCookie.tsx
+++ client/src/pages/pages_without_cookie/ProductViewWithoutCookie.tsx
@@ -4,15 +4,18 @@ import LoadingSpinner from '../../components/LoadingSpinner';
 import ErrorMessage from '../../components/ErrorMessage';
 import Filter from '../../components/Filter';
 import { useSearchParams } from 'react-router-dom';
+import Pagination from '../../components/Pagination';
 
 const ProductViewWithoutCookie = () => {
   const [searchParams] = useSearchParams();
   const name = searchParams?.get('name') || null;
   const category = searchParams?.get('category');
+  const page = parseInt(searchParams.get('page') || '1', 10);
 
-  const { products, error, isLoading } = useProductsWithoutCookie(
+  const { products, pagination, error, isLoading } = useProductsWithoutCookie(
     name,
-    category
+    category,
+    page
   );
 
   if (isLoading) {
@@ -26,9 +29,9 @@ const ProductViewWithoutCookie = () => {
   return (
     <>
       <Filter resetLink="/without-cookie/products" />
-      {/* <h3 className="text-primary text-3xl font-bold text-right">
+      <h3 className="text-primary text-3xl font-bold text-right">
         Total: {pagination?.totalItems} Produk
-      </h3> */}
+      </h3>
       <div className="md:grid-cols-3 lg:grid-cols-4 grid grid-cols-2 gap-5 mt-5">
         {!products?.length ? (
           <h1 className="col-span-full text-3xl font-bold">
@@ -49,6 +52,11 @@ const ProductViewWithoutCookie = () => {
           ))
         )}
       </div>
+      {pagination && (
+        <div className="flex justify-center mt-5">
+          <Pagination pagination={pagination} />
+        </div>
+      )}
     </>
   );
 };
